8fc6af3620546488356992d25ae294222407c715
Albert Dong <albertdong2011@gmail.com>
2022-12-04 22:05:52 -0800

    comments for utility.hpp

--- utility.hpp
+++ utility.hpp
@@ -1,6 +1,7 @@
 #include <string>
 using namespace std;
 
+// Gets next int from an input string and deletes the part of string up to and including the int
 bool getNext(string& s, int& output) {
     string valid = "0123456789";
     int start = s.find_first_of(valid), end = s.find_first_not_of(valid, start);
@@ -10,6 +11,7 @@ bool getNext(string& s, int& output) {
     return true;
 }
 
+// Gets next alphabetical char from an input string and deletes the part of string up to and including the char
 bool getNext(string& s, char& output) {
     string valid = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM";
     int loc = s.find_first_of(valid);
@@ -19,6 +21,7 @@ bool getNext(string& s, char& output) {
     return true;
 }
 
+// Gets next alphabetical string from an input string and deletes the part of string up to and including the string
 bool getNext(string& s, string& output) {
     string valid = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM";
     int start = s.find_first_of(valid), end = s.find_first_not_of(valid, start);
