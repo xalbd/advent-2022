b44aa21e984f4ca92db1fc95846492a992eb62aa
Albert Dong <albertdong2011@gmail.com>
2022-12-10 13:16:16 -0800

    removal of final array in 10b

--- 10/10b.cpp
+++ 10/10b.cpp
@@ -9,10 +9,14 @@
 #include "../utility.hpp"
 using namespace std;
 
-void run(int &cycle, int x, char crt[6][40]) {  // x: 0-39, cycle: 1-40
+void run(int &cycle, int x) {  // x: 0-39, cycle: 1-40
+    if (cycle % 40 == 1) cout << endl;
     cycle--;
     if (abs((cycle % 40) - x) <= 1 && cycle < 240) {
-        crt[cycle / 40][cycle % 40] = '#';
+        cout << "#";
+    }
+    else {
+        cout << " ";
     }
     cycle += 2;
 }
@@ -22,34 +26,19 @@ void solve(string filename) {
     string s;
     vector<string> parsed;
     int cycle = 1, x = 1;
-    char crt[6][40];
-
-    for (auto &a : crt) {
-        for (auto &b : a) {
-            b = ' ';
-        }
-    }
 
     while (getline(file, s)) {
         if (s == "") break;
         parsed = parse(s, " ");
         if (s == "noop") {
-            run(cycle, x, crt);
+            run(cycle, x);
         }
         else {
-            run(cycle, x, crt);
-            run(cycle, x, crt);
+            run(cycle, x);
+            run(cycle, x);
             x += stoi(parsed[1]);
         }
     }
-
-    for (auto &a : crt) {
-        for (auto &b : a) {
-            cout << b;
-        }
-        cout << endl;
-    }
-    cout << endl;
 
     file.close();
 }
